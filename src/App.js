040e4bc900dfea14c98bdf31f5d451a67299603d
balaji <balajinia@gmail.com>
2022-01-19 15:56:27 +0530

    master branch

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1>counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged ? <h3>Valuable information i shouldn't see </h3> : ""}
+    </div>
+  );
+}
+
+export default App;
